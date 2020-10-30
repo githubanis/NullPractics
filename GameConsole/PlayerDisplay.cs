513a491b6acb179657fca32db611da6d5f3b4721
anis_gitlab <anisurrahman.bs23@gmail.com>
2020-10-30 21:39:47 +0600

    Add project files.

---
+++ GameConsole/PlayerDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    class PlayerDisplay
+    {
+        public static void Write(PlayerCharacter player)
+        {
+            Console.WriteLine(player.Name);
+
+            if (player.DaysSinceLastLogin == -1)
+            {
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            }
+            else
+            {
+                Console.WriteLine(player.DaysSinceLastLogin);
+            }
+
+            if (player.DateOfBrith == DateTime.MinValue)
+            {
+
+            }
+        }
+    }
+}
---
+++ GameConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace GameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var p = new PlayerDisplay();
+            var a = new PlayerCharacter();
+            p.Write(a);
+        }
+    }
+}
