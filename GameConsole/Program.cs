a6f0b43fa0ed41a9091f6d40d9688c7027ff775d
anis_gitlab <anisurrahman.bs23@gmail.com>
2020-10-31 23:21:02 +0600

    A

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -9,19 +9,14 @@ namespace GameConsole
         static void Main(string[] args)
         {
             var player = new PlayerCharacter();
-            while (true)
-            {
-                Console.WriteLine("Enter Player Name: ");
-                player.Name = Console.ReadLine();
-                Console.WriteLine("Enter Player DayScienceLastLogin: ");
-                player.DaysSinceLastLogin = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Player DOB: ");
-                player.DateOfBrith = Convert.ToDateTime(Console.ReadLine());
-                Console.WriteLine("Enter Player Experience: ");
-                player.IsNoob = Convert.ToBoolean(Console.ReadLine());
+            player.Name = "    Anis  ";
+            player.Name.Trim();
+            player.DaysSinceLastLogin = 23;
+            player.DateOfBrith = DateTime.Today;
+            player.IsNoob = true;
 
-                player.Print();
-            }
+            player.Print();
+
         }
     }
 }
